e1e5350861dd8897d93a3d3485ddf5042570ea85
Kenia Valladarez <keniavalladarez@gmail.com>
2017-01-25 21:20:13 -0500

    Adds wireframe for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "naw_pages/home"
+
+  root "nav_pages#home"
 end

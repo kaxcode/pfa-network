fa25ba5765a5b1ff051bc3d579f35c2deae14ea0
Kenia Valladarez <keniavalladarez@gmail.com>
2017-01-25 22:09:39 -0500

    Adds the wireframe layout for the login/ sign up page for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "naw_pages/home"
+  get "nav_pages/home"
 
-  get "naw_pages/about"
+  get "nav_pages/about"
+
+  get "nav_pages/login"
 
   root "nav_pages#about"
 end

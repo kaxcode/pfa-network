b610f74cb911a9e452cd41adf19c4416f0f510b6
Kenia Valladarez <keniavalladarez@gmail.com>
2017-01-30 17:16:18 -0500

    Adds the comment scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   # Factory method to create a user from some omniauth data
   # Omniauth will use this to build a *NEW* user for us
   def self.from_omniauth(authentication_data)

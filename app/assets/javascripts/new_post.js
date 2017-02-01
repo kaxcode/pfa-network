04d5bf2add08ab9c8d0beb09cffa0ec952ee0711
Kenia Valladarez <keniavalladarez@gmail.com>
2017-02-01 09:19:58 -0500

    Rials migration to add category id to posts table

--- app/assets/javascripts/new_post.js
+++ app/assets/javascripts/new_post.js
@@ -1,9 +1,11 @@
-// $('.dropdown-menu a').click(function(){
-//    $('#selected').text($(this).text());
-//  });
+$(document).on("turbolinks:load", function(){
+ $(".category-dropdown-menu li").click(function() {
+   var id = $(this).data("id");
 
- $(document).ready(function(){
-     $(".dropdown-menu li a").click(function(){
-     $('#selected').text($(this).text());
-     });
+   $(".category-select").val(id);
+ })
+
+   $(".dropdown-menu li a").click(function(){
+   $('#selected').text($(this).text());
+   });
  });
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -57,6 +57,6 @@ class PostsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def post_params
-      params.require(:post).permit(:title, :body, :user_id)
+      params.require(:post).permit(:title, :body)
     end
 end

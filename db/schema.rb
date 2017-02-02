0afd43e56007443711dcd7fed00fe3a4d3ab56c3
Kenia Valladarez <keniavalladarez@gmail.com>
2017-02-01 22:36:05 -0500

    Rails migration to add admin column to users table with boolean deafult to fault

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201142153) do
+ActiveRecord::Schema.define(version: 20170202033356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,8 +48,9 @@ ActiveRecord::Schema.define(version: 20170201142153) do
     t.string   "uid"
     t.string   "nickname"
     t.string   "access_token"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.boolean  "admin",        default: false
   end
 
   add_foreign_key "posts", "users"

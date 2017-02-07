82b4aa1884ee5a8bba1437bc56a67f7ca86019ee
Kenia Valladarez <keniavalladarez@gmail.com>
2017-02-07 13:43:21 -0500

    Updates the seed file with more posts and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,11 @@ penney_streetman = User.create!(name: "Penney Streetman")
 carmelo_meinecke = User.create!(name: "Carmelo Meinecken")
 elodia_kennell = User.create!(name: "Elodia Kennell")
 tyree_shuff = User.create!(name: "TyreeShuff")
-# clemmie_cianciolo  = User.create!(name: "Clemmie Cianciolo")
-# millicent_enderle  = User.create!(name: "Millicent Enderle")
-# dodie_crooms       = User.create!(name: "Dodie Crooms")
-# michel_berube      = User.create!(name: "Michel Berube")
-# xavier_paez        = User.create!(name: "Xavier Paez")
+clemmie_cianciolo = User.create!(name: "Clemmie Cianciolo")
+millicent_enderle = User.create!(name: "Millicent Enderle")
+dodie_crooms = User.create!(name: "Dodie Crooms")
+michel_berube = User.create!(name: "Michel Berube")
+xavier_paez = User.create!(name: "Xavier Paez")
 
 # Creates Topics
 general                       = Topic.create!(name: "General")
@@ -98,7 +98,7 @@ post_9 = Post.create!(
 post_10 = Post.create!(
   title: "Family Network for Disabilities",
   body: "http://fndusa.org/",
-  user: chelsea_baret ,
+  user: chelsea_baret,
   topic: general,
 )
 
@@ -107,4 +107,46 @@ post_11 = Post.create!(
   body: "https://www.understood.org/en",
   user: eda_thibodaux,
   topic: general,
+)
+
+post_12 = Post.create!(
+  title: "My Child's IEP",
+  body: "Topping topping carrot cake toffee. Croissant dragée marshmallow muffin caramels oat cake bear claw sesame snaps. Icing halvah marzipan jelly beans lemon drops sweet roll cake marzipan lemon drops. Apple pie tiramisu sesame snaps tootsie roll jelly-o dessert chocolate cake cheesecake tiramisu. Powder bear claw jelly sesame snaps icing lollipop. Marshmallow dragée chocolate bar marzipan croissant biscuit sugar plum caramels oat cake. Fruitcake ice cream dragée marshmallow tiramisu topping candy. Cheesecake marshmallow pudding dragée jelly marshmallow pastry. Pastry oat cake sweet sweet roll. Jelly beans cake marshmallow carrot cake jujubes brownie chocolate cake. Cheesecake cheesecake lollipop gingerbread jujubes macaroon pudding dessert lollipop. Sweet sugar plum bear claw gummi bears. Candy jelly carrot cake fruitcake brownie jelly beans cookie. Gingerbread marshmallow jujubes pudding caramels candy canes soufflé jelly-o.",
+  user: clemmie_cianciolo,
+  topic: individualized_education_plan,
+)
+
+post_13 = Post.create!(
+  title: "My Child's Evaluations",
+  body: "Topping topping carrot cake toffee. Croissant dragée marshmallow muffin caramels oat cake bear claw sesame snaps. Icing halvah marzipan jelly beans lemon drops sweet roll cake marzipan lemon drops. Apple pie tiramisu sesame snaps tootsie roll jelly-o dessert chocolate cake cheesecake tiramisu. Powder bear claw jelly sesame snaps icing lollipop. Marshmallow dragée chocolate bar marzipan croissant biscuit sugar plum caramels oat cake. Fruitcake ice cream dragée marshmallow tiramisu topping candy. Cheesecake marshmallow pudding dragée jelly marshmallow pastry. Pastry oat cake sweet sweet roll. Jelly beans cake marshmallow carrot cake jujubes brownie chocolate cake. Cheesecake cheesecake lollipop gingerbread jujubes macaroon pudding dessert lollipop. Sweet sugar plum bear claw gummi bears. Candy jelly carrot cake fruitcake brownie jelly beans cookie. Gingerbread marshmallow jujubes pudding caramels candy canes soufflé jelly-o.",
+  user: millicent_enderle,
+  topic: evaluations,
+)
+
+post_14 = Post.create!(
+  title: "Southwest Elementary Waiting List",
+  body: "Topping topping carrot cake toffee. Croissant dragée marshmallow muffin caramels oat cake bear claw sesame snaps. Icing halvah marzipan jelly beans lemon drops sweet roll cake marzipan lemon drops. Apple pie tiramisu sesame snaps tootsie roll jelly-o dessert chocolate cake cheesecake tiramisu. Powder bear claw jelly sesame snaps icing lollipop. Marshmallow dragée chocolate bar marzipan croissant biscuit sugar plum caramels oat cake. Fruitcake ice cream dragée marshmallow tiramisu topping candy. Cheesecake marshmallow pudding dragée jelly marshmallow pastry. Pastry oat cake sweet sweet roll. Jelly beans cake marshmallow carrot cake jujubes brownie chocolate cake. Cheesecake cheesecake lollipop gingerbread jujubes macaroon pudding dessert lollipop. Sweet sugar plum bear claw gummi bears. Candy jelly carrot cake fruitcake brownie jelly beans cookie. Gingerbread marshmallow jujubes pudding caramels candy canes soufflé jelly-o.",
+  user: dodie_crooms,
+  topic: school_waiting_lists,
+)
+
+post_15 = Post.create!(
+  title: "Dentist Waiting List",
+  body: "Topping topping carrot cake toffee. Croissant dragée marshmallow muffin caramels oat cake bear claw sesame snaps. Icing halvah marzipan jelly beans lemon drops sweet roll cake marzipan lemon drops. Apple pie tiramisu sesame snaps tootsie roll jelly-o dessert chocolate cake cheesecake tiramisu. Powder bear claw jelly sesame snaps icing lollipop. Marshmallow dragée chocolate bar marzipan croissant biscuit sugar plum caramels oat cake. Fruitcake ice cream dragée marshmallow tiramisu topping candy. Cheesecake marshmallow pudding dragée jelly marshmallow pastry. Pastry oat cake sweet sweet roll. Jelly beans cake marshmallow carrot cake jujubes brownie chocolate cake. Cheesecake cheesecake lollipop gingerbread jujubes macaroon pudding dessert lollipop. Sweet sugar plum bear claw gummi bears. Candy jelly carrot cake fruitcake brownie jelly beans cookie. Gingerbread marshmallow jujubes pudding caramels candy canes soufflé jelly-o.",
+  user: michel_berube,
+  topic: medical_waiting_lists,
+)
+
+post_16 = Post.create!(
+  title: "My Blog- Xavier",
+  body: "Topping topping carrot cake toffee. Croissant dragée marshmallow muffin caramels oat cake bear claw sesame snaps. Icing halvah marzipan jelly beans lemon drops sweet roll cake marzipan lemon drops. Apple pie tiramisu sesame snaps tootsie roll jelly-o dessert chocolate cake cheesecake tiramisu. Powder bear claw jelly sesame snaps icing lollipop. Marshmallow dragée chocolate bar marzipan croissant biscuit sugar plum caramels oat cake. Fruitcake ice cream dragée marshmallow tiramisu topping candy. Cheesecake marshmallow pudding dragée jelly marshmallow pastry. Pastry oat cake sweet sweet roll. Jelly beans cake marshmallow carrot cake jujubes brownie chocolate cake. Cheesecake cheesecake lollipop gingerbread jujubes macaroon pudding dessert lollipop. Sweet sugar plum bear claw gummi bears. Candy jelly carrot cake fruitcake brownie jelly beans cookie. Gingerbread marshmallow jujubes pudding caramels candy canes soufflé jelly-o.",
+  user: xavier_paez,
+  topic: blogs,
+)
+
+post_17 = Post.create!(
+  title: "My Blog- Penney",
+  body: "Topping topping carrot cake toffee. Croissant dragée marshmallow muffin caramels oat cake bear claw sesame snaps. Icing halvah marzipan jelly beans lemon drops sweet roll cake marzipan lemon drops. Apple pie tiramisu sesame snaps tootsie roll jelly-o dessert chocolate cake cheesecake tiramisu. Powder bear claw jelly sesame snaps icing lollipop. Marshmallow dragée chocolate bar marzipan croissant biscuit sugar plum caramels oat cake. Fruitcake ice cream dragée marshmallow tiramisu topping candy. Cheesecake marshmallow pudding dragée jelly marshmallow pastry. Pastry oat cake sweet sweet roll. Jelly beans cake marshmallow carrot cake jujubes brownie chocolate cake. Cheesecake cheesecake lollipop gingerbread jujubes macaroon pudding dessert lollipop. Sweet sugar plum bear claw gummi bears. Candy jelly carrot cake fruitcake brownie jelly beans cookie. Gingerbread marshmallow jujubes pudding caramels candy canes soufflé jelly-o.",
+  user: penney_streetman,
+  topic: blogs,
 )
